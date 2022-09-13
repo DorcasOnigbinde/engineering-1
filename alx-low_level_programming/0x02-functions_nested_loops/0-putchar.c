e490dacca639b64c07a474e2d9834af4144c0084
DorcasOnigbinde <dorcasoluwapelumi18@gmail.com>
2022-09-13 08:32:59 -0700

    add _putchar

---
+++ alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>                                                                                                                                     
+#include <stdio.h>                                                                                                                                     
+                                                                                                                                                       
+/* this function prints _putchar */                                                                                                                                                     
+int main(void) {
+	printf("_putchar\n");
+	return (0);
+}  
